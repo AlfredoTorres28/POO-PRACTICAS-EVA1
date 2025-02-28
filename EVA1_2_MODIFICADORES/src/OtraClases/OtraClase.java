b2142011adc010a7cdf16f403a2f6b63f3f4d750
AlfredoTorres28 <159494258+AlfredoTorres28@users.noreply.github.com>
2025-02-28 09:34:03 -0600

    Add files via upload

---
+++ EVA1_2_MODIFICADORES/src/OtraClases/OtraClase.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package OtraClases;
+
+/**
+ *
+ * @author jealt
+ */
+public class OtraClase {
+    public int valorpublico;
+    private int valorprivado;
+    int valorDefault;
+}
+
+class Prueba{
+    
+}
---
+++ EVA1_2_MODIFICADORES/src/eva1_2_modificadores/EVA1_2_MODIFICADORES.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_2_modificadores;
+
+import OtraClases.OtraClase;
+
+/**
+ *
+ * @author jealt
+ */
+public class EVA1_2_MODIFICADORES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        OtraClase otraClase = new OtraClase();
+        
+    
+    }
+    
+}
